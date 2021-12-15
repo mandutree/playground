43aee7839b40184e0b725334039af612f532f1fa
Steven Han <namhee@amazon.com>
2021-12-14 20:19:04 -0800

    are you old enough to drink?

---
+++ src/main/java/com/play/MainApp.java
@@ -0,0 +1,24 @@
+package com.play;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("What is your age? ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        line = line.trim();
+        int age = Integer.parseInt(line);
+        int ageRequirement = 19;
+
+        if (age < 0) {
+            System.out.println("invalid age");
+        } else if (age > 100) {
+            System.out.println("are you really that old?");
+        } else if (age < ageRequirement) {
+            System.out.println("you are too young to drink");
+        } else {
+            System.out.println("you are old enough to drink");
+        }
+    }
+}
