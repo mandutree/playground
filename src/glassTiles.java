cb7819b87791f6fb93e50efce4926783988008b4
Steven Han <namhee@amazon.com>
2021-12-27 21:42:24 -0800

    refactored the code to simplify

--- src/glassTiles.java
+++ src/glassTiles.java
@@ -1,42 +1,32 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class glassTiles {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String correctTile = "";
-        String wrongTile = "";
-        String guess = "";
-        int correctGuess = 0;
+        Random random = new Random();
+        int correctCounter = 0;
 
-        while (true){
-            if (guess.equals(wrongTile)){
+        while (true) {
+            boolean correctTile = random.nextBoolean();
+            System.out.println("Choose left or right: ");
+            String guess = sc.nextLine();
+
+            if (!guess.equalsIgnoreCase("left") && !guess.equalsIgnoreCase("right")) {
+                System.out.println("That's not a choice. Choose left or right, or die!");
+                continue;
+            }
+
+            if (guess.equalsIgnoreCase("left") == correctTile) {
+                if (++correctCounter > 4) {
+                    System.out.println("You survived the game!!!");
+                    break;
+                }
+                System.out.println("you survived the round, continue");
+            } else {
+                System.out.println("you fell to your death");
                 break;
             }
-                int random = (int)(Math.random() * 2); // Math.random() gives a random value between 0 - 0.99 etc.
-                System.out.println("Choose left or right: ");
-                guess = sc.nextLine();
-
-                if (random == 0){
-                    correctTile = "left";
-                    wrongTile = "right";
-                }
-                else {
-                    correctTile = "right";
-                    wrongTile = "left";
-                }
-
-                if (!guess.equalsIgnoreCase("left") && !guess.equalsIgnoreCase("right")) {
-                    System.out.println("That's not a choice. Choose left or right, or die!");
-                }
-                else {
-                    if (guess.equalsIgnoreCase(correctTile)){
-                        System.out.println("you survived, continue");
-                        correctGuess++;
-                    }
-                    else {
-                        System.out.println("you fell to your death");
-                    }
-                }
-            }
+        }
     }
 }
